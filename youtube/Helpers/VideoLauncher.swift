40a23d492f151e361051563a2c993605cee730bf
Lewis Le <phuonghoangle@gmail.com>
2017-11-23 09:54:59 +0700

    Display video player controls

--- youtube/Helpers/VideoLauncher.swift
+++ youtube/Helpers/VideoLauncher.swift
@@ -10,20 +10,92 @@ import UIKit
 import AVFoundation
 import AVKit
 
-class VideoPlayer: UIView {
+class VideoPlayerView: UIView {
+    
+    let activityIndicatorView: UIActivityIndicatorView = {
+        let aiv = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        aiv.translatesAutoresizingMaskIntoConstraints = false
+        aiv.startAnimating()
+        return aiv
+    }()
+    
+    lazy var pausePlayButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "pause")
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        
+        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    var isPlaying = false
+    
+    @objc func handlePause() {
+        if isPlaying {
+            player?.pause()
+            pausePlayButton.setImage(UIImage(named: "play"), for: .normal)
+        } else {
+            player?.play()
+            pausePlayButton.setImage(UIImage(named: "pause"), for: .normal)
+        }
+        
+        isPlaying = !isPlaying
+    }
+    
+    let controlsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        setupPlayerView()
+        
+        controlsContainerView.frame = frame
+        addSubview(controlsContainerView)
+        
+        controlsContainerView.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        controlsContainerView.addSubview(pausePlayButton)
+        pausePlayButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        pausePlayButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        pausePlayButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        pausePlayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
         backgroundColor = .black
-        let urlString = "https://firebasestorage.googleapis.com/v0/b/youtapevideos.appspot.com/o/Taylor%20Swift%20-%20Blank%20Space.mp4?alt=media&token=1c5f062b-e1d1-43d5-8248-7082a508a2f1"
-
+    }
+    
+    var player: AVPlayer?
+    
+    fileprivate func setupPlayerView() {
+        let urlString = "https://firebasestorage.googleapis.com/v0/b/youtapevideos.appspot.com/o/Lambo.mp4?alt=media&token=28a0aaf6-8406-41dc-bb40-992c7a1e50d0"
+        
         if let url = URL(string: urlString) {
-            let player = AVPlayer(url: url)
+            player = AVPlayer(url: url)
             let playerLayer = AVPlayerLayer(player: player)
             self.layer.addSublayer(playerLayer)
             playerLayer.frame = self.frame
-
-            player.play()
+            
+            player?.play()
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+        }
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        // Player is ready and rendering frames
+        if keyPath == "currentItem.loadedTimeRanges" {
+            activityIndicatorView.stopAnimating()
+            controlsContainerView.backgroundColor = .clear
+            pausePlayButton.isHidden = false
+            isPlaying = true
         }
     }
     
@@ -43,7 +115,7 @@ class VideoLauncher: NSObject {
             
             let height = keyWindow.frame.width * 9 / 16
             let videoPlayerFrame = CGRect(x: 0, y: 0, width: keyWindow.frame.width, height: height)
-            let videoPlayer = VideoPlayer(frame: videoPlayerFrame)
+            let videoPlayer = VideoPlayerView(frame: videoPlayerFrame)
             view.addSubview(videoPlayer)
             
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
